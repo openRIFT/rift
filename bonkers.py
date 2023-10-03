ea056ae3ceff5420f983ff3a15e01ba0343538c3
Stormy <80485413+Kinda-Stormy@users.noreply.github.com>
2023-10-03 06:42:53 -0700

    Add highlighting for a file.
    Also shorten debug command 'debug.skipdownload' to 'db.skip'

--- bonkers.py
+++ bonkers.py
@@ -7,6 +7,7 @@ import os
 import keyboard
 import cursor
 import time
+import subprocess
 
 # Variables
 listItem = 0
@@ -14,11 +15,62 @@ listItem = 0
 # Clear Screen
 os.system('cls')
 
-# Functions
+# Functions ↓
+
+# Welcome Screen
 def welcomeScreen():
     print(Back.MAGENTA + "Welcome to Bonkers v0.1.0")
     print(Style.RESET_ALL)
-
+ 
+# Bonk downloader    
+def downloadBonkAppList():
+    global bonkURL
+    bonkURL = input("Provide a file repo: ")
+    
+    # If skipDownload is enabled
+    if bonkURL == 'db.skip':
+        print(Back.RED + 'Beware, this command can crash Bonkers!', Style.RESET_ALL)
+        time.sleep(0.5)
+        return
+    
+    os.system('cls')
+    print(Fore.YELLOW + 'Loading...')
+    r = requests.get(bonkURL, allow_redirects=True)
+    open('userFiles/list.bal', 'wb').write(r.content)
+    
+# Bonk list
+def bonkLister():
+    
+    os.system('cls')
+    
+    with open('list.bal', 'r') as f:
+        lines = len(f.readlines())
+        lines = str(lines)
+        
+    print(Back.LIGHTGREEN_EX, Fore.BLACK + lines + " files available", Back.RED, Fore.WHITE + "ESC to close", Back.YELLOW, Fore.BLACK + bonkURL, Style.RESET_ALL)
+    print(Style.RESET_ALL + "-" * 45)
+    lines = int(lines)
+    
+    with open('list.bal', 'r') as f:
+        app = f.readlines()
+        
+    for i in range(lines):
+        appList = app[i]
+        appItem = appList.split(';')
+        appItem = appItem[0].replace('\n', '')
+        
+        if i == listItem:
+            global appURL
+            appList = appList.split(';')
+            appURL = appList[1]
+            appURL = appURL.replace('\n', '')
+            print(Fore.GREEN + appItem, Back.WHITE, Fore.MAGENTA + "")
+            
+        else:
+            print(Style.RESET_ALL + appItem)
+            
+    print(Style.RESET_ALL + "-" * 45)
+    
 # Bonk key listener        
 def bonkKeyListener():
     with open('list.bal', 'r') as f:
@@ -45,59 +97,8 @@ def bonkKeyListener():
             os.system("cls")
             exit(0)
             
-        time.sleep(0.1)        
- 
-# Bonk downloader    
-def downloadBonkAppList():
-    global bonkURL
-    bonkURL = input("Provide a file repo: ")
-    
-    # If skipDownload is enabled
-    if bonkURL == 'debug.skipdownload':
-        print(Back.RED + 'Beware, this command can crash Bonkers!', Style.RESET_ALL)
-        time.sleep(1)
-        return
-    
-    os.system('cls')
-    print(Fore.YELLOW + 'Loading...')
-    r = requests.get(bonkURL, allow_redirects=True)
-    open('list.bal', 'wb').write(r.content)
-    
-# Bonk list
-def bonkLister():
-    
-    os.system('cls')
-    
-    with open('list.bal', 'r') as f:
-        lines = len(f.readlines())
-        lines = str(lines)
-        
-    print(Back.LIGHTGREEN_EX, Fore.BLACK + lines + " files available", Back.RED, Fore.WHITE + "ESC to close", Back.YELLOW, Fore.BLACK + bonkURL, Style.RESET_ALL)
-    print(Style.RESET_ALL + "--------------------")
-    lines = int(lines)
-    
-    with open('list.bal', 'r') as f:
-        app = f.readlines()
-        
-    for i in range(lines):
-        appList = app[i]
-        appItem = appList.split(';')
-        appItem = appItem[0].replace('\n', '')
-        
-        if i == listItem:
-            global appURL
-            appList = appList.split(';')
-            appURL = appList[1]
-            appURL = appURL.replace('\n', '')
-            print(Fore.GREEN + appItem)
-            
-        else:
-            print(Style.RESET_ALL + appItem)
-            
-    print(Style.RESET_ALL + "--------------------")
-    cursor.hide()
-            
-            
+        time.sleep(0.1)            
+                   
 # Call Weclome Screen and run the Bonk downloader
 welcomeScreen()
 downloadBonkAppList()
