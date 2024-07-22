6ede3a297900a37359706f36b2ef034ef6a32c01
Stormy <80485413+0hStormy@users.noreply.github.com>
2024-07-22 15:35:25 -0700

    Update rift.py

--- rift.py
+++ rift.py
@@ -43,7 +43,7 @@ def makeConfig():
 
         "ExecDownloadWarn": True,
 
-        "DownloadsFolder": "@HOME/Documents/",
+        "DownloadsFolder": "@HOME/Downloads/",
         "ProgramFiles": "rift/"
     }
 
